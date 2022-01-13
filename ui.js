efe7a0dd47a5ad095fcb78b0917c1acfc2e35893
WalterOfNone <walterofnone@protonmail.com>
2022-01-13 14:01:19 -0600

    Update ui.js
    removed unneeded code

--- ui.js
+++ ui.js
@@ -1,19 +1,5 @@
 // Initialize button with user's preferred color
-let changeColor = document.getElementById("changeColor");
 let showSolution = document.getElementById("showSolution")
-
-chrome.storage.sync.get("color", ({ color }) => {
-  changeColor.style.backgroundColor = color;
-});
-// When the button is clicked, inject setPageBackgroundColor into current page
-changeColor.addEventListener("click", async () => {
-    let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-  
-    chrome.scripting.executeScript({
-      target: { tabId: tab.id },
-      function: setPageBackgroundColor,
-    });
-});
 
 showSolution.addEventListener("click", async () => {
     let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
@@ -27,11 +13,3 @@ showSolution.addEventListener("click", async () => {
 function showSolution() {
     document
 }
-
-  
-  // The body of this function will be executed as a content script inside the
-  function setPageBackgroundColor() {
-    chrome.storage.sync.get("color", ({ color }) => {
-      document.body.style.backgroundColor = color;
-    });
-  }
